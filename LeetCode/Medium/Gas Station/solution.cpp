1f5ee631f31756bbe2f7cf98a9dbc27cf336c265
Nidhi Goyal <g.nidhi2355@gmail.com>
2026-08-30 22:19:44 +0530

    [Syncode] LeetCode/Medium/Gas Station

---
+++ LeetCode/Medium/Gas Station/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total_gas = 0, current_gas = 0, start_index = 0;
+        
+        for (int i = 0; i < gas.size(); i++) {
+            int diff = gas[i] - cost[i];
+            total_gas += diff;
+            current_gas += diff;
+            
+            if (current_gas < 0) {
+                start_index = i + 1;
+                current_gas = 0;
+            }
+        }
+        
+        return total_gas >= 0 ? start_index : -1;
+    }
+};
