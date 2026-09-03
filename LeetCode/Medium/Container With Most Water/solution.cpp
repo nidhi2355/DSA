6f012ba4b541e7108293fefc03625bc5c97b69ac
Nidhi Goyal <g.nidhi2355@gmail.com>
2026-09-03 20:15:23 +0530

    [Syncode] LeetCode/Medium/Container With Most Water

---
+++ LeetCode/Medium/Container With Most Water/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans=0;
+
+        int i=0, j= height.size()-1;
+
+        while(i<j){
+            int h= min(height[i], height[j]);
+            int w= j-i;
+
+            ans= max(ans, h*w);
+
+            if(height[i]< height[j]) i++;
+            else j--;
+        }
+
+        return ans;
+    }
+};
